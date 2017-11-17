11f0832686f91444d9bbad4a6594b59fbd5ff882
Manivannan Guru <31209650+yogumanivannan@users.noreply.github.com>
2017-11-17 10:16:24 +0530

    Add files via upload

--- event.js
+++ event.js
@@ -2,7 +2,9 @@ console.log('initializing...');
 
 const serverless = require('serverless-http');
 const express = require('express');
+//const cors = require('cors');
 const appEvent = express();
+//appEvent.use(cors());
 
 Sequelize = require('sequelize');
 Op = Sequelize.Op;
@@ -23,9 +25,14 @@ appEvent.get('/event', (req, res) => {
     //requestHandler = new requestResponse(req, res);
     eReq = req;
     eRes = res;
+    //common.log('---->');
+    //common.log('---->'+eReq);
     const eventController = require('./module/event/controller/event');
     let eventControllerObj = new eventController();
-    eventControllerObj.getEvent();
+    eventControllerObj.getEvent().then(function(response){
+        eRes.status(response.status);
+        eRes.send(response);
+    });
     common.log('exiting...')
     //res.send("I'm fine!");
 });
--- module/event/controller/event.js
+++ module/event/controller/event.js
@@ -8,8 +8,19 @@ class event{
     getEvent(){
         let eventId = [1,2];
         let eventModelObject = new eventModel(eventId);
-        eventModelObject.getEvent().then(function(result){
-            eRes.send(result);
+        return eventModelObject.getEvent().then(function(result){
+            var response = {
+                status: 200,
+                data : result
+            }
+            //eRes.status(200);
+            //eRes.send(response);
+
+            var promise = new Promise(function(resolve, reject){
+                resolve(response);
+            });
+
+            return promise;
         });
     }
 }
